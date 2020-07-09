431ff3098ddcf32fe70fb127cfe8ec04185a3386
BenitezDev <benitezdev@gmail.com>
2020-07-09 13:52:44 +0200

    fix form and add data to db

--- src/models/Event.js
+++ src/models/Event.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
-    title: String,
-    max_capacity: Int16Array,
-    current_capacity: Int16Array,
-    price: Float32Array
+    title: String
 
+    // only accept Strings ¯\_(ツ)_/¯
+
+    // max_capacity: Int16Array
+    // current_capacity: Int16Array,
+    // price: Float32Array
 });
 
 module.exports = mongoose.model('events', EventSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-//const Evento = require('../models/Event');
+const Evento = require('../models/Event');
 
 
 router.get('/', (req, res) => {
@@ -8,16 +8,14 @@ router.get('/', (req, res) => {
 })
 
 router.get('/get', (req, res) => {
-
     res.render('index')
 })
 
 router.post('/event', async (req, res) => {
-    //var data = req.body;
-    //const event = new Evento(req.body);
-    //await event.save();
+
+    const event = new Evento(req.body);
+    await event.save();
     console.log(req.body);
-    res.send('received')
 })
 
 module.exports = router;
