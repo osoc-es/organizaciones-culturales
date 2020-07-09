a2466d357afa15778d4110694a1fb19d1cea4aaa
MAGMa <36223494+miguelangelgil@users.noreply.github.com>
2020-07-09 13:16:50 +0200

    form

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
     title: String,
     max_capacity: Int16Array,
     current_capacity: Int16Array,
     price: Float32Array
+
 });
 
-module.exports = mongoose.model('eventos', EventSchema);
+module.exports = mongoose.model('events', EventSchema);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+//const Evento = require('../models/Event');
 
 
 router.get('/', (req, res) => {
     res.render('index')
 })
 
+router.get('/get', (req, res) => {
+
+    res.render('index')
+})
+
+router.post('/event', async (req, res) => {
+    //var data = req.body;
+    //const event = new Evento(req.body);
+    //await event.save();
+    console.log(req.body);
+    res.send('received')
+})
+
 module.exports = router;
+
