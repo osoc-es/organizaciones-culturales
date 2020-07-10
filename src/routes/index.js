2ed35fa504ab86089af27229dbded92b4868ce69
Talto Canelo <52244393+TaltoCanelo@users.noreply.github.com>
2020-07-10 11:44:51 +0200

    page3

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,27 +1,30 @@
 const express = require('express');
 const router = express.Router();
-const Event = require('../models/Event');
+const Evento = require('../models/Event');
 
-router.get('/', async (req, res) => {
-    const events = await Event.find();
-    res.render('index', { events });
+
+router.get('/', (req, res) => {
+    res.render('index')
 })
 
-
-router.get('/home', (req, res) => {
-    res.render('home')
+router.get('/get', (req, res) => {
+    res.render('index')
 })
+
 router.get('/page-2', (req, res) => {
     res.render('page-2')
 })
 
-router.post('/event', async (req, res) => {
-    const event = new Event(req.body);
-    await event.save();
-    res.redirect('/');
-    console.log(req.body);
+router.get('/page3', (req, res) => {
+    res.render('page3')
 })
 
+router.post('/event', async (req, res) => {
+
+    const event = new Evento(req.body);
+    await event.save();
+    console.log(req.body);
+})
 
 module.exports = router;
 
