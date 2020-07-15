6cadb534f867c7d9e3cc4f0b08217329ec774bd6
BenitezDev <benitezdev@gmail.com>
2020-07-15 11:48:37 +0200

    get data from events in home view

--- src/models/Categories.js
+++ src/models/Categories.js
@@ -1,10 +1,10 @@
 
 const categories = new Map();
 
-categories.set(0,"CINE");
-categories.set(1,"TEATRO");
-categories.set(2,"GASTRONOMIA");
-categories.set(3,"MUSEO");
+categories.set(0, "CINE");
+categories.set(1, "TEATRO");
+categories.set(2, "GASTRONOMIA");
+categories.set(3, "MUSEO");
 
 
 module.exports = categories;
