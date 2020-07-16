7aaae21d7cca96edd4c3d3f835f9d9d9676bfe99
BenitezDev <benitezdev@gmail.com>
2020-07-16 15:42:10 +0200

    search view, and fix event view

--- src/models/Categories.js
+++ src/models/Categories.js
@@ -3,9 +3,9 @@ const categories = new Map();
 
 categories.set(0, "CINE");
 categories.set(1, "TEATRO");
-categories.set(2, "GASTRONOMÍA");
-categories.set(3, "MUSEO");
-categories.set(4, "MÚSICA");
+
+categories.set(2, "MUSEO");
+categories.set(3, "MÚSICA");
 
 
 module.exports = categories;
